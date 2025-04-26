23c85155ef732fdc6305989620aa539f61439103
nobody-cya <cya.chen.ya@gmail.com>
2025-04-26 18:52:52 +0800

    Update wezterm.lua

--- common/wezterm/wezterm.lua
+++ common/wezterm/wezterm.lua
@@ -2,29 +2,29 @@ local wezterm = require("wezterm")
 local config = {}
 
 wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
+  local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
 end)
 
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.font_size = 9.00
-	config.font = wezterm.font("JetBrainsMono Nerd Font")
-	config.default_prog = { "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" }
-	config.window_background_opacity = 0.90
+  config.font_size = 9.00
+  config.font = wezterm.font("JetBrainsMono Nerd Font")
+  config.default_prog = { "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" }
+  config.window_background_opacity = 0.90
 elseif wezterm.target_triple == "aarch64-apple-darwin" then
-	config.font_size = 13.00
-	config.font = wezterm.font("JetBrains Mono")
-	config.default_prog = { "zsh", "-l" }
-	config.window_background_opacity = 0.90
+  config.font_size = 13.00
+  config.font = wezterm.font("JetBrains Mono")
+  config.default_prog = { "zsh", "-l" }
+  config.window_background_opacity = 0.90
 elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
-	config.font_size = 12.00
-	config.font = wezterm.font("JetBrainsMono Nerd Font")
-	config.default_prog = { "bash", "-l" }
-	config.window_background_opacity = 0.80
+  config.font_size = 12.00
+  config.font = wezterm.font("JetBrainsMono Nerd Font")
+  config.default_prog = { "bash", "-l" }
+  config.window_background_opacity = 0.80
 end
 config.color_scheme = "Default Dark (base16)"
 config.line_height = 1.00
