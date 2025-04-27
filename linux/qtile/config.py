089604ee4281228ecc95e5a5387997eb05a75cf8
nobody-cya <cya.chen.ya@gmail.com>
2025-04-26 21:33:22 -0400

    Add files via upload

---
+++ linux/qtile/config.py
@@ -0,0 +1,184 @@
+import os
+import subprocess
+from libqtile import bar, layout, qtile, widget, hook
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+
+@hook.subscribe.startup_once
+def autostart():
+    commands = [
+        "dunst",
+        "xclip",
+        "nm-applet",
+        "flameshot",
+        "blueman-applet",
+        "udiskie --tray",
+        "/usr/lib/polkit-kde-authentication-agent-1",
+        "xrdb -merge /home/cya/.Xresources",
+        "ibus-daemon -drxR",
+        "redshift -x && redshift -O 4500",
+    ]
+    for cmd in commands:subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+mod = "mod4"
+keys = [
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod, "control"], "h", lazy.layout.grow_left(), lazy.layout.grow().when(layout=["monadtall"])),
+    Key([mod, "control"], "l", lazy.layout.grow_right(), lazy.layout.shrink().when(layout=["monadtall"])),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), lazy.layout.grow().when(layout=["monadtall"])),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), lazy.layout.shrink().when(layout=["monadtall"])),
+    Key([mod], "Return", lazy.spawn("kitty"), desc="Launch terminal"),
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod, "shift"], "q", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen on the focused window"),
+    Key([mod, "shift"], "f", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+    Key([mod, "shift"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "shift"], "e", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "space", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod, "shift"], "space", lazy.spawn("rofi -show run"), desc="Launch terminal"),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%"), desc="Raise Volume"),
+    Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%"), desc="Lower Volume"),
+    Key([], "XF86AudioMute", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle"), desc="Audio Mute"),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl s +5%"), desc="Lower Monitor Brightness"),
+    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl s 5%-"), desc="Raise Monitor Brightness")
+]
+
+for vt in range(1, 8):
+    keys.append(
+        Key(
+            ["control", "mod1"],
+            f"f{vt}",
+            lazy.core.change_vt(vt).when(func=lambda: qtile.core.name == "wayland"),
+            desc=f"Switch to VT{vt}",
+        )
+    )
+
+
+groups = [Group(i) for i in "1234567890"]
+
+for i in groups:
+    keys.extend(
+        [
+            Key([mod], i.name, lazy.group[i.name].toscreen(), desc=f"Switch to group {i.name}"),
+            Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True), desc=f"Switch to & move focused window to group {i.name}"),
+        ]
+    )
+
+layouts = [
+    # layout.Columns(),
+    # layout.Max(),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+    layout.MonadTall(border_width=4, margin=4, border_focus="#a1b56c", border_normal="#585858"),
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    # layout.Tile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font="JetBrainsMono Nerd Font Semibold",
+    fontsize=15,
+    padding=3,
+    background="#181818",
+    foreground="#d8d8d8",
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        wallpaper = "/home/cya/wallpaper/wp1.jpg",
+        wallpaper_mode = "fill",
+        bottom=bar.Bar(
+            [
+                widget.Prompt(cursor_color="#ab4642", prompt="Run: "),
+                widget.CurrentLayoutIcon(scale=0.75),
+                widget.GroupBox(highlight_method="border", active="#ab4642", inactive="#585858", this_current_screen_border="#585858"),
+                widget.WindowName(),
+                widget.TextBox(text="  ", foreground="#dc9656"),
+                widget.CPU(format="{load_percent:.0f}%"),
+                widget.TextBox(text="  ", foreground="#86c1b9"),
+                widget.Memory(format="{MemPercent:.0f}%"),
+                widget.TextBox(text=" 󰕾 ", foreground="#d8d8d8"),
+                widget.Volume(fmt="{}"),
+                widget.TextBox(text="  ", foreground="#f7ca88"),
+                widget.Backlight(format="{percent:2.0%}", backlight_name="intel_backlight"),
+                widget.TextBox(text=" 󱈏 ", foreground="#a1b56c"),
+                widget.Battery(format="{percent:2.0%}"),
+                widget.TextBox(text=" 󰃰 ", foreground="#ba8baf"),
+                widget.Clock(format="%Y-%m-%d %H:%M"),
+                widget.Systray(),
+            ],
+            30,
+            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+        ),
+        # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
+        # By default we handle these events delayed to already improve performance, however your system might still be struggling
+        # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
+        # x11_drag_polling_rate = 60,
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: list
+follow_mouse_focus = True
+bring_front_click = False
+floats_kept_above = True
+cursor_warp = False
+floating_layout = layout.Floating(
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# When using the Wayland backend, this can be used to configure input devices.
+wl_input_rules = None
+
+# xcursor theme (string or None) and size (integer) for Wayland backend
+wl_xcursor_theme = None
+wl_xcursor_size = 24
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
